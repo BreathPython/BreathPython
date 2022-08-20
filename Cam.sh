c9468276e6d6aa4ff41393d097cefdbdf87e1c2d
Najeeb Ullah <64611793+BreathPython@users.noreply.github.com>
2022-08-20 14:18:12 +0500

    Update Cam.sh

--- Cam.sh
+++ Cam.sh
@@ -4,3 +4,14 @@
 # Credits goes to Najeeb Ullah -BreathPython and Hamza Qayyum [github.com/BreathPython/]
 
 trap 'printf "\n";stop' 2
+clear
+printf "\033[1;97m                  *     \e[0m\n"
+printf "\033[1;97m    (     (      (  `    \e[0m\n"
+printf "\033[1;96m    )\    )\     )\))(     \e[0m\n"
+printf "\033[1;95m  (((_)((((_)(  ((_)()\   \e[0m\n"
+printf "\033[1;94m  )\___ )\ _ )\ (_()((_)   \e[0m\n"
+printf "\033[1;93m ((/ __|(_)_\(_)|  \/  |  \e[0m\n"
+printf "\033[1;92m  | (__  / _ \  | |\/| |  e[0m\n"
+printf "\033[1;91m   \___|/_/ \_\ |_|  |_|  e[0m\n"
+printf "\033[1;93m    WhatsApp: +923124788959    \e[0m\n\n"
+printf "\033[1;95m insta@itx.najeeb Facebook:fb.com/OyeJanu500\e[0m\n\n"
